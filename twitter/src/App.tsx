c4f45a70372d5276d8597e64cc9ef0f3808a04ef
hhu <hhu@fortinet.com>
2020-07-20 23:52:19 -0700

    code format

--- twitter/src/App.tsx
+++ twitter/src/App.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from "react";
-import Routes from "Routes";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-import "./App.less";
+import React, { FC } from 'react';
+import Routes from 'Routes';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import './App.less';
 
 const App: FC = () => (
   <Provider store={store}>
