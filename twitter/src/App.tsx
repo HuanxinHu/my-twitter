b8c573f82b941170825aaea70d91fe63798db6a9
hhu <hhu@fortinet.com>
2020-07-22 16:47:35 -0700

    tune app

--- twitter/src/App.tsx
+++ twitter/src/App.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import Routes from "Routes";
-import "./App.less";
-import store from "./store";
+import React, { FC } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from 'Routes';
+import './App.less';
+import store from './store';
 
 const App: FC = () => (
   <Provider store={store}>
